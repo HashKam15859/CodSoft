519f0d6102a73b25eab2ca484932e9fdf0cf315f
HashKam <157140391+HashKam15859@users.noreply.github.com>
2025-03-11 03:07:55 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -6,8 +6,11 @@ def password_generator (length, strength):
     characters = ''
     if strength == 1:
         characters = string.ascii_letters
-    else:
+    elif strength == 2: 
         characters = string.ascii_letters + string.digits + string.punctuation
+    else:   # If the user inputs anything besides 1 or 2 for strength, program will exit
+        print("Invalid choice for strength! Exiting.....")
+        exit()
     password = ''.join(random.choices(characters, k=length)) 
     return password
 
@@ -17,24 +20,24 @@ checker = 0 # Checker variable used to exit from the loop
 # Exception Handling to handle any invalid inputs entered by the user
 try:
     while checker == 0:
-        length = int(input("Enter the desired password length: "))
+        length = int(input("\nEnter the desired password length: "))
+        print("What kind of password would you like? \n1. Weak Password (Consists of only letters) \n2. Strong Password (Consists of letters, digits & special characters)\n")
+        strength = int(input("Enter a choice: "))
         if length < 8: 
             if length < 1: 
                 print("A password of this length is not possible! Try again~")
                 continue
             else: 
-                print("What kind of password would you like? \n1. Weak Password (Consists of only letters) \n2. Strong Password (Consists of letters, digits & special characters)\n")
-                strength = int(input("Enter a choice: "))
                 print("A good password has atleast 8 characters. Would you still like to proceed with a short length password? (y/n): ")
                 checker2 = input().lower()
                 if checker2 == 'n':
                     continue
                 else:
-                    print("Generated Password: ", password_generator(length, strength))
+                    print("\nGenerated Password: ", password_generator(length, strength))
                     checker = 1
         else:
-            print("Generated Password: ", password_generator(length, strength))
+            print("\nGenerated Password: ", password_generator(length, strength))
             checker = 1
-except ValueError:  # Program will stop if an invalid input is entered
+except ValueError:
     print("Invalid input! Exiting from the Password Generator.....")
   
